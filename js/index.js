78a5d2a56b2706908dcd047f0f90775455ebb65c
jair <jair.neto.angeluci@hotmail.com>
2026-09-14 17:49:27 -0300

    #time 10m - new visuals for index

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
-// icons
-var icons = document.getElementsByClassName("icon");
-
-for (var a = 0; a < icons.length; a++) {
-    icons[a].onmouseover = function () {
-        this.style["filter"] = "opacity(0.5) drop-shadow(0 0 0 white) brightness(250%)";
-    }
-}
-
-for (var a = 0; a < icons.length; a++) {
-    icons[a].onmouseout = function () {
-        this.style["filter"] = "opacity(0.5) drop-shadow(0 0 0 white)";
-    }
+// Ano atual no rodapé
+var yearEl = document.getElementById("year");
+if (yearEl) {
+	yearEl.textContent = new Date().getFullYear();
 }
